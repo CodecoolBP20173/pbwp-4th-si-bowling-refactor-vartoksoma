90d3b8f4ab68d6d7f2c114b89522f157af754877
Vartok Soma <vartok.soma@gmail.com>
2017-11-26 20:01:49 +0100

    further refactored

--- bowling.py
+++ bowling.py
@@ -1,51 +1,47 @@
+#  if u are unfamiliar with bowling rules, go check wiki
 Spare = '/'
-Strike = "X"
 strike = "x"
 Invalid = '-'
 STRIKESCORE = 10
 
 
 def score(game):
-    result = 0
-    frame = 1
+    result, frame = 0, 1
     in_first_half = True
-    for turn in range(len(game)):
-        if game[turn] == Spare:
+    max_gamenum = 10
+    for roll in range(len(game)):
+        if game[roll] == Spare:
             result += STRIKESCORE - last
         else:
-            result += get_value(game[turn])
-        if frame < STRIKESCORE and get_value(game[turn]) == STRIKESCORE:
-            if game[turn] == Spare:
-                result += get_value(game[turn+1])
-            elif game[turn] == Strike or game[turn] == strike:
-                result += get_value(game[turn+1])
-                if game[turn+2] == Spare:
-                    result += STRIKESCORE - get_value(game[turn+1])
+            result += get_value(game[roll])
+        if frame < max_gamenum and get_value(game[roll]) == STRIKESCORE:
+            if game[roll] == Spare:
+                result += get_value(game[roll+1])
+            elif game[roll].lower() == strike:
+                result += get_value(game[roll+1])
+                frame += 1
+                if game[roll+2] == Spare:
+                    result += STRIKESCORE - get_value(game[roll+1])
                 else:
-                    result += get_value(game[turn+2])
-        last = get_value(game[turn])
+                    result += get_value(game[roll+2])
+        last = get_value(game[roll])
         if not in_first_half:
             frame += 1
         if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[turn] == Strike or game[turn] == strike:
+        if game[roll].lower() == strike:
             in_first_half = True
-            frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    try:
         return int(char)
-    elif char == Strike or char == strike:
-        return STRIKESCORE
-    elif char == Spare:
-        return STRIKESCORE
-    elif char == Invalid:
-        return 0
-    else:
+    except ValueError:
+        if char.lower() == strike or char == Spare:
+            return STRIKESCORE
+        elif char == Invalid:
+            return 0
         raise ValueError()
